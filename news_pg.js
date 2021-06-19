77eef115894074ed2d50132765b341e7150119ad
utkarshpr <46813085+utkarshpr@users.noreply.github.com>
2021-06-19 11:46:52 +0530

    Update news_pg.js

--- news_pg.js
+++ news_pg.js
@@ -12,7 +12,7 @@ logo.addEventListener('click', news_function);
 
 //a6f30bf16cc241dda6f2810d1c019a02
 let country =
-  'https://gnews.io/api/v4/top-headlines?lang=en&country=us&token=5715f6772710bd3964536456a5c47e12';
+  'https://gnews.io/api/v4/top-headlines?lang=en&country=us&token=';
 let new_group = document.getElementById('new_group');
 //create a get request
 const xhr = new XMLHttpRequest();
